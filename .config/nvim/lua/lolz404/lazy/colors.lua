3c380dfa8e1629dbe7569c5deb75f6bd557b24ba
Coury Richards <2702992+couryrr@users.noreply.github>
2024-06-12 09:14:03 -0400

    updated to lazy package manager

---
+++ .config/nvim/lua/lolz404/lazy/colors.lua
@@ -0,0 +1,29 @@
+function ColorMyNvim(color)
+	color = color or "tokyonight-storm"
+	vim.cmd.colorscheme(color)
+
+
+end
+
+return {
+    {
+        "folke/tokyonight.nvim",
+        config = function()
+            require("tokyonight").setup({
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+                styles = {
+                    -- Style to be applied to different syntax groups
+                    -- Value is any valid attr-list value for `:help nvim_set_hl`
+                    comments = { italic = false },
+                    keywords = { italic = false },
+                    -- Background styles. Can be "dark", "transparent" or "normal"
+                    sidebars = "dark", -- style for sidebars, see below
+                    floats = "dark", -- style for floating windows
+                },
+            })
+            ColorMyNvim()
+        end
+    }
+}
--- nvim/lua/lolz404/init.lua
+++
@@ -1,3 +0,0 @@
-require("lolz404.remap")
-require("lolz404.set")
-
