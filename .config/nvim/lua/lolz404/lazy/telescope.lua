d79dc198cf4e4827b38cd8b312dfcd7f082aa75d
Coury Richards <2702992+couryrr@users.noreply.github>
2024-06-15 12:11:37 -0400

    wip staging

--- .config/nvim/lua/lolz404/lazy/telescope.lua
+++ .config/nvim/lua/lolz404/lazy/telescope.lua
@@ -2,27 +2,32 @@ return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
     dependencies = {
-        "nvim-lua/plenary.nvim"
+      "nvim-lua/plenary.nvim",
+      "jonarrien/telescope-cmdline.nvim",
     },
-
-    config = function()
+    keys = {
+        { ':', '<cmd>Telescope cmdline<cr>', desc = 'Cmdline' }
+    },
+    config = function(_, opts)
         require('telescope').setup({})
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>fp', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>fws', function()
+        vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
         end)
-        vim.keymap.set('n', '<leader>fWs', function()
+        vim.keymap.set('n', '<leader>pWs', function()
             local word = vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
         end)
-        vim.keymap.set('n', '<leader>fs', function()
+        vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+
+        require("telescope").setup(opts)
+        require("telescope").load_extension('cmdline')
     end
 }
 
