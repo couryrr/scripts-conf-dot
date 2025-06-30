2124ed85d88cd2d18c6cc61a48224a99ac096895
Coury Richards <2702992+couryrr@users.noreply.github.com>
2025-06-29 22:31:53 -0400

    clean up
    Signed-off-by: Coury Richards <2702992+couryrr@users.noreply.github.com>

--- linux/.config/nvim/lua/plugins/luasnip.lua
+++ linux/.config/nvim/lua/plugins/luasnip.lua
@@ -3,12 +3,5 @@ return {
     event = "InsertEnter",
     config = function()
         require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/lua/snippets" })
-        -- vim.keymap.set({ "i", "s" }, "<Tab>", function()
-        --     return require("luasnip").jump(1)
-        -- end, { silent = true })
-        --
-        -- vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
-        --     return require("luasnip").jump(-1)
-        -- end, { silent = true })
-    end,
+   end,
 }
