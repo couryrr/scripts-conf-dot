1a57482dbcabd532dfafbbea568dd558a87fdde8
Coury Richards <2702992+couryrr@users.noreply.github.com>
2026-04-21 12:00:35 -0400

    Update the way bashrc is managed - Moved user configured settings to .config file - Removed duplicate files - Removed unused plugins and apps

--- linux/config/.config/nvim/lua/plugins/llm.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "supermaven-inc/supermaven-nvim",
-    config = function()
-        require("supermaven-nvim").setup({})
-        vim.keymap.set("n", "<leader>cs", function()
-            vim.cmd("SupermavenToggle")
-        end, { desc = "Toggle Supermaven" })
-    end,
-}
